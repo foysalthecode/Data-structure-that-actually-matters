2197d01f1c4a72c8b971cecf5a71562cb78e74fe
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-05 14:25:14 +0600

    Practice: Queue implementation

---
+++ queue.js
@@ -0,0 +1,52 @@
+//? Queue --> FIFO (first in first out)
+
+//? enqueue --> it means give input in queue
+//? dequeue --> it means kick the element out
+
+//* Array implementation
+
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  //* O(1)
+  enqueue(value) {
+    this.items.push(value);
+  }
+
+  //* O(n)
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items.shift();
+  }
+
+  //* O(1)
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[0];
+  }
+
+  //* O(1)
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  //* O(n)
+  print() {
+    console.log("Start -->", this.items.join(" --> "), "--> End");
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.print();
+queue.dequeue();
+queue.print();
