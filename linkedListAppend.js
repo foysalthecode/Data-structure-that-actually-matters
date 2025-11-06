6150c54518df397c1b68cb7c0e65c684271194f7
foysalthecode <ahmedmdfoysal123@gmail.com>
2025-11-06 11:30:49 +0600

    Practice: insert inplementation

--- linkedListAppend.js
+++ linkedListAppend.js
@@ -22,6 +22,7 @@ class LinkedList {
       this.tail = newNode;
     }
     this.length++;
+    return this;
   }
   prepend(value) {
     const newNode = new Node(value);
@@ -35,9 +36,44 @@ class LinkedList {
       this.head = newNode;
     }
     this.length++;
+    return this;
   }
-  insert() {}
+  insert(index, value) {
+    if ((index < 0, index > this.length)) {
+      console.log("Index out of boundery");
+      return undefined;
+    }
+    //if the insert is in the start of the linked list
+    if (index === 0) {
+      return this.prepend(value);
+    }
+
+    //if the insert is in the end of the linked list
+    if (index === this.length) {
+      return this.append(value);
+    }
+
+    //if the insert is in the middle of the linked list
+    const leadingNode = this._traverseToIndex(index - 1);
+    const holdingNode = leadingNode.next;
+    const newNode = new Node(value);
+    leadingNode.next = newNode;
+    newNode.next = holdingNode;
+    this.length++;
+  }
   remove() {}
+
+  //private helper method
+  _traverseToIndex(index) {
+    let count = 0;
+    let currentNode = this.head;
+    while (count != index) {
+      currentNode = currentNode.next;
+      count++;
+    }
+    return currentNode;
+  }
+
   print() {
     const arr = [];
     let currentNode = this.head;
@@ -51,13 +87,8 @@ class LinkedList {
 
 const linkedlist = new LinkedList();
 
-linkedlist.append(1);
-linkedlist.append(2);
-linkedlist.append(3);
+linkedlist.append(1).append(2).append(3);
 
-
-linkedlist.prepend(10)
-linkedlist.prepend(20)
-linkedlist.prepend(30)
+linkedlist.insert(2, 100);
 
 linkedlist.print();
